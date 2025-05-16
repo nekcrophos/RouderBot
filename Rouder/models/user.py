c68e2d59aca53afbb35d72970417ec39ae68dd0e
nekcrophos <golfboywang@gmail.com>
2025-05-16 22:50:26 +0300

    Выбор интересов сделан (Не добавляется текст)

--- Rouder/models/user.py
+++ Rouder/models/user.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, telegram_id: int = None, name: str = None, surname: str = None, age: int = 0, avatar: str = None, description: str = None, register = False):
+    def __init__(self, telegram_id: int = None, name: str = None, surname: str = None, age: int = 0, avatar: str = None, description: str = None, register = False,  interests = {'music': [], 'place': [], 'actives': [], 'pop_culter': [], 'lifestyle': []}):
         self.avatar = avatar
         self.telegram_id = telegram_id
         self.name = name
@@ -7,5 +7,6 @@ class User:
         self.description = description
         self.age = age
         self.register = register
+        self.interests = interests
     def __str__(self):
         return f'User(telegram_id: {self.telegram_id}, name: {self.name}, surname: {self.surname}, age: {self.age}, description: {self.decription})'
