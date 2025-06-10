79d490c570ecfff1428f9c85c585538f94ddccf2
ksirkl <ksirkl@email.com>
2025-06-10 15:50:39 +0300

    add feedback model

---
+++ Rouder/models/feedback.py
@@ -0,0 +1,10 @@
+from baseModel import *
+from user import User
+
+class Feedbacks(BaseModel):
+    id = AutoField(column_name = "id")
+    from_id = ForeignKeyField(User)
+    to_id = ForeignKeyField(User)
+    like = BooleanField(column_name="like")
+    class Meta:
+        table_name = "Feedbacks"
